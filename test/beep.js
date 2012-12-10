06a63cee132d179d1552b550fb64ef33f51a8762
James Halliday <mail@substack.net>
2012-12-10 11:33:45 -0800

    initial thing

---
+++ test/beep.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+require('should');
+
+var squareModule = require('../');
+
+describe('integers', function () {
+    it('should square the numbers', function () {
+        // you can use asserts
+        assert.equal(squareModule(2), 4);
+        assert.equal(squareModule(3), 9);
+        
+        // or should-style assertions work too
+        squareModule(5).should.eql(25);
+    });
+});
